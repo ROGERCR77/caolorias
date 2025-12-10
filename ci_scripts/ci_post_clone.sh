3094ee68338911adf92f83c873e40040116aca2d
ROGERCR77 <rogercontato94@gmail.com>
2025-12-10 08:32:40 -0300

    Fix ci_post_clone script and permissions

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,6 +4,38 @@ set -e
 # Ir para a raiz do repositório clonado pelo Xcode Cloud
 cd "$CI_WORKSPACE"
 
+# 1) Instalar dependências Node (onde fica @capacitor/ios)
+if [ -f package-lock.json ]; then
+  npm ci
+else
+  npm install
+fi
+
+# 2) Ir para o projeto iOS e instalar os Pods
+cd iOS/App
+pod install
+#!/bin/sh
+set -e
+
+# Ir para a raiz do repositório clonado pelo Xcode Cloud
+cd "$CI_WORKSPACE"
+
+# 1) Instalar dependências Node (onde fica @capacitor/ios)
+if [ -f package-lock.json ]; then
+  npm ci
+else
+  npm install
+fi
+
+# 2) Ir para o projeto iOS e instalar os Pods
+cd iOS/App
+pod install
+#!/bin/sh
+set -e
+
+# Ir para a raiz do repositório clonado pelo Xcode Cloud
+cd "$CI_WORKSPACE"
+
 # 1) Instalar dependências Node (Capacitor precisa disso para gerar os headers)
 if [ -f package-lock.json ]; then
   npm ci
